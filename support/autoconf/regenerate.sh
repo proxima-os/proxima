ac5a1f4894cf56aaed4fba7d7154a4ca386832bb
monkuous <84319635+monkuous@users.noreply.github.com>
2025-01-28 18:43:01 +0100

    ensure consistent autoreconf ordering

--- support/autoconf/regenerate.sh
+++ support/autoconf/regenerate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -ue
 
-find . '(' -name configure.ac -o -name configure.in ')' -type f -print0 | xargs -0 autoreconf -fvi "$@"
+find . '(' -name configure.ac -o -name configure.in ')' -type f -print0 | sort -z | xargs -0 autoreconf -fvi "$@"
